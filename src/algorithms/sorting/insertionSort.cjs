59b39e0f8cb7efde35eb700b5a1c7fa3ef4207fd
HarshDev1809 <harsh.dev182000@gmail.com>
2025-04-04 22:47:09 +0530

    insertion sort added

---
+++ src/algorithms/sorting/insertionSort.cjs
@@ -0,0 +1,30 @@
+// src/insertion-sort.cjs
+function insertionSort(array, order = "asc") {
+    if (!array || !Array.isArray(array) || array.length === 0) {
+        throw new Error("Please provide a valid array for sorting!");
+    }
+
+    for (let i = 1; i < array.length; i++) {
+        let key = array[i];
+        let j = i - 1;
+        if(order === "asc"){
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }else if(order === "desc"){
+            while (j >= 0 && array[j] < key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }else{
+            throw new Error("Invalid sorting order given");
+        }
+    }
+
+    return array;
+}
+
+module.exports = insertionSort;
